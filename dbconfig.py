f97392ccab60fb427dd62cf15bf1f3b0a4f1cb2a
markcot <g00376335@gmit.ie>
2020-12-07 14:07:45 +0000

    Database sql table intialisation added

---
+++ dbconfig.py
@@ -0,0 +1,6 @@
+mysql={
+    'host':"localhost",
+    'username':'root',
+    'password':'Root147!',
+    'database':'company'
+}
---
+++ initdb.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE company;
+
+USE company;
+
+CREATE TABLE department(
+	deptID INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+    location varchar(255) DEFAULT NULL,
+    budget INT DEFAULT NULL,
+    PRIMARY KEY (deptID)
+    );    
+
+CREATE TABLE employee(
+	empID INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+    address VARCHAR(255) DEFAULT NULL,
+    salary INT DEFAULT NULL,
+    dept INT DEFAULT NULL,
+    PRIMARY KEY (empID),
+    CONSTRAINT FK_deptEmp
+		FOREIGN KEY (dept)
+		REFERENCES department(deptID)
+    );
