cf4a963eb34382da204e343acb7b0d247deb44c3
markcot <g00376335@gmit.ie>
2020-12-23 16:49:34 +0000

    server: CRUD ops logging added. Login: page redirection to index updated

--- README.md
+++ README.md
@@ -30,15 +30,15 @@ This database is stored in an sql database, which has an initialisation file 'in
 The DAO for the 'company' database has been created in the class file 'CompanyDao.py', which creates an instance of the class called 'companyDao' that can be used in with an associated python server to undertake CRUD operations for the sql database. The sql database login configuration specific for each machine and user are stored in the 'dbconfig.py' file.
 
 ### Flask server
-A python server called 'server.py' has been created using Python flask for undertaking CRUD operations in association with the 'company' sql DAO. The server can be run from the command line by typing the command 'python server.py'
+A python server called 'server.py' has been created using Python flask for undertaking CRUD operations in association with the 'company' sql DAO. The server can be run from the command line by typing the command 'python server.py'. The server includes some basic server logging of the database CRUD operations.
 
 ### App Login
-A basic login page 'login.html' is included in the 'staticpages' folder has been added to the front end of the app. The login page is accessible via the root address "/" of the app (localhost 'http://127.0.0.1:5000/'). The submitted login form does not have a secure authentication for the password as it is visible in URL and Google Chrome gives a warning about this. A more more secure method of login authentication should be undertaken if more time was available to implement it. Allowable "user : password" combinations are:
+A basic login page 'login.html' is included in the 'staticpages' folder has been added to the front end of the app. The login page is accessible via the root address "/" of the app (localhost 'http://127.0.0.1:5000/'). The submitted login form does not have a secure authentication for the password. A more more secure method of login authentication should be undertaken if more time was available to implement it. Allowable "user : password" combinations are:
 * admin : admin
 * mark : 1234
 * andrew : abcd
 
-### Index GUI page
+### App Index GUI page
 A HTML user interface page 'Index.html' is included the 'staticpages' folder. This Graphical User Interface (GUI) can be used to view and undertake CRUD operations on the database content. Once the flask server is running, the GUI can be accessed from a web browser after logging in at the login page.
 
 ### Virtual environment
@@ -60,5 +60,3 @@ The package requirements can also be install from the list in the file 'requirem
 
 ### Deployment to pythonanywhere.com
 The database has been deployed to pythonanywhere at the address http://markcotter.pythonanywhere.com/.
-
-
--- server.py
+++ server.py
@@ -1,5 +1,23 @@
 from flask import Flask, url_for, request, redirect, abort, jsonify
 from CompanyDao import companyDao
+from logging.config import dictConfig
+
+# Logger config. Code verbatim from https://flask.palletsprojects.com/en/1.1.x/logging/
+dictConfig({
+   'version': 1,
+   'formatters': {'default': {
+      'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+   }},
+   'handlers': {'wsgi': {
+      'class': 'logging.StreamHandler',
+      'stream': 'ext://flask.logging.wsgi_errors_stream',
+      'formatter': 'default'
+   }},
+   'root': {
+      'level': 'INFO',
+      'handlers': ['wsgi']
+   }
+})
 
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
 
@@ -18,42 +36,49 @@ def login():
 # curl http://127.0.0.1:5000/departments
 @app.route('/departments')
 def getAllDept():
+   app.logger.info('Get all depts')
    return jsonify(companyDao.getAllDept())
 
 #get all employees route
 # curl http://127.0.0.1:5000/employees
 @app.route('/employees')
 def getAllEmp():
+   app.logger.info('Get all emps')
    return jsonify(companyDao.getAllEmp())
 
 #get all users route (userID and name only)
-# curl http://127.0.0.1:5000/users
+# curl http://127.0.0.1:5000/u
 @app.route('/u')
 def getAllUser():
+   app.logger.info('Get all users')
    return jsonify(companyDao.getAllUser())
 
 # find By department id route
 # curl http://127.0.0.1:5000/departments/1
 @app.route('/departments/<int:deptID>')
 def findDeptById(deptID):
+   app.logger.info('Get dept %s', deptID)
    return jsonify(companyDao.findDeptById(deptID))
 
 # find By employee id route
 # curl http://127.0.0.1:5000/employees/1
 @app.route('/employees/<int:empID>')
 def findEmpById(empID):
+   app.logger.info('Get emp %s', empID)
    return jsonify(companyDao.findEmpById(empID))
 
 #get all employees by department id route
 # curl http://127.0.0.1:5000/employees/dept/1
 @app.route('/employees/dept/<int:deptID>')
 def getAllEmpByDept(deptID):
+   app.logger.info('Get all emps in dept %s', deptID)
    return jsonify(companyDao.getAllEmpByDept(deptID))
 
 # find By user id route
 # curl http://127.0.0.1:5000/u/1
 @app.route('/u/<int:userID>')
 def findUserByID(userID):
+   app.logger.info('Get user %s', userID)
    return jsonify(companyDao.findUserByID(userID))
 
 # create department route
@@ -62,12 +87,14 @@ def findUserByID(userID):
 @app.route('/departments', methods=['POST'])
 def createDept():
    if not request.json:
+      app.logger.info('Request format not json')
       abort(400)
    dept = {
       "name": request.json["name"],
       "location": request.json["location"],
       "budget": request.json["budget"]
    }
+   app.logger.info('Created dept %s', dept)
    return jsonify(companyDao.createDept(dept))
 
 # create employee route
@@ -77,11 +104,13 @@ def createDept():
 @app.route('/employees', methods=['POST'])
 def createEmp():
    if not request.json:
+      app.logger.info('Request format not json')
       abort(400)
    # check if dept exists
    dept = request.json["dept"]
    checkDept = companyDao.findDeptById(dept)
    if checkDept == {}:
+      app.logger.info('DeptID %s not found', checkDept.deptID)
       return jsonify({}), 404
    emp = {
       "name": request.json["name"],
@@ -89,6 +118,7 @@ def createEmp():
       "salary": request.json["salary"],
       "dept": request.json["dept"]
    }
+   app.logger.info('Created emp %s', emp)
    return jsonify(companyDao.createEmp(emp))
 
 # create user route
@@ -96,11 +126,13 @@ def createEmp():
 @app.route('/u', methods=['POST'])
 def createUser():
    if not request.json:
+      app.logger.info('Request format not json')
       abort(400)
    u = {
       "name": request.json["name"],
       "password": request.json["password"]
    }
+   app.logger.info('Created user %s', user)
    return jsonify(companyDao.createUser(u))
 
 #update department route
@@ -110,6 +142,7 @@ def updateDept(deptID):
    foundDept = companyDao.findDeptById(deptID)
    # print(foundDept)
    if foundDept == {}:
+      app.logger.info('DeptID %s not found', deptID)
       return jsonify({}), 404
    currentDept = foundDept
    if 'name' in request.json:
@@ -119,6 +152,7 @@ def updateDept(deptID):
    if 'budget' in request.json:
       currentDept['budget'] = request.json['budget']
    companyDao.updateDept(currentDept)
+   app.logger.info('Updated dept %s', currentDept)
    return jsonify(currentDept)
 
 #update employee route
@@ -128,6 +162,7 @@ def updateEmp(empID):
    foundEmp = companyDao.findEmpById(empID)
    # print(foundEmp)
    if foundEmp == {}:
+      app.logger.info('EmpID %s not found', empID)
       return jsonify({}), 404
    currentEmp = foundEmp
    if 'name' in request.json:
@@ -141,9 +176,11 @@ def updateEmp(empID):
       deptID = request.json['dept']
       checkDept = companyDao.findDeptById(deptID)
       if checkDept == {}:
+         app.logger.info('DeptID %s not found', deptID)
          return jsonify({}), 404
       currentEmp['dept'] = deptID
    companyDao.updateEmp(currentEmp)
+   app.logger.info('Updated emp %s', currentEmp)
    return jsonify(currentEmp)
 
 #update user route
@@ -153,6 +190,7 @@ def updateUser(userID):
    foundUser = companyDao.findUserByID(userID)
    print(foundUser)
    if foundUser == {}:
+      app.logger.info('UserID %s not found', userID)
       return jsonify({}), 404
    currentUser = foundUser
    if 'name' in request.json:
@@ -160,6 +198,7 @@ def updateUser(userID):
    if 'password' in request.json:
       currentUser['password'] = request.json['password']
    companyDao.updateUser(currentUser)
+   app.logger.info('Updated user %s', currentUser)
    return jsonify(currentUser)
 
 #delete department route
@@ -169,9 +208,15 @@ def deleteDept(deptID):
    # check if dept exists
    foundDept = companyDao.findDeptById(deptID)
    if foundDept == {}:
+      app.logger.info('DeptID %s not found', deptID)
       return jsonify({"done": False}), 404
-   companyDao.deleteDept(deptID)
-   return jsonify({"done": True})
+   try:
+      companyDao.deleteDept(deptID)
+      app.logger.info('Deleted dept %s', foundDept)
+      return jsonify({"done": True})
+   except:
+      app.logger.info('Cannot delete deptID %s', deptID)
+      return jsonify({"done": False})
 
 #delete employee route
 # curl -X DELETE http://127.0.0.1:5000/employees/1
@@ -180,8 +225,10 @@ def deleteEmp(empID):
    # check if emp exists
    foundEmp = companyDao.findEmpById(empID)
    if foundEmp == {}:
+      app.logger.info('EmpID %s not found', empID)
       return jsonify({"done": False}), 404
    companyDao.deleteEmp(empID)
+   app.logger.info('Deleted emp %s', foundEmp)
    return jsonify({"done": True})
 
 #delete user route
@@ -191,8 +238,10 @@ def deleteUser(userID):
    # check if dept exists
    foundUser = companyDao.findUserByID(userID)
    if foundUser == {}:
+      app.logger.info('UserID %s not found', userID)
       return jsonify({"done": False}), 404
    companyDao.deleteUser(userID)
+   app.logger.info('Deleted user %s', foundUser)
    return jsonify({"done": True})
 
 if __name__ == "__main__":
